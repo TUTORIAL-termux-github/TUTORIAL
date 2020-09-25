d3daab91f0a613e4f0a44226845fa7c20989c533
TUTORIAL-termux-github <71549156+TUTORIAL-termux-github@users.noreply.github.com>
2020-09-25 20:47:03 +0700

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,12 @@
+clear
+figlet R3D BULL  | lolcat
+echo
+__________________________________________________________
+
+#nama   : R3D BULL BAR²
+#chanel : R3D BULL CYBER
+
+__________________________________________________________
+ | lolcat
+
+sleep 2
